2f4f1deeb4c134e687316c83ac5dcd858163a807
tursunbaevz <zuma9405@gmail.com>
2016-09-10 15:34:40 +0300

    Work on Active Admin

--- app/admin/post.rb
+++ app/admin/post.rb
@@ -13,5 +13,35 @@ ActiveAdmin.register Post do
 #   permitted
 # end
 
+	
+    form do |f|
+      f.inputs do
+      f.input :description
+      f.input :image, :as => :file, :hint => image_tag(f.object.image.url(:thumb))
+    end
+    f.actions
+  end
+
+    index do
+    selectable_column
+    id_column
+    column :image do |post|
+      image_tag post.image.url(:thumb)
+    end
+    column :title do |post|
+      link_to post.title, admin_post_path(post)
+    end
+    actions
+  end
+
+	 show do
+	 attributes_table do
+	   row :image do |post|
+	     image_tag post.image.url(:medium)
+	   end
+	   row :description
+	 end
+	 active_admin_comments
+	end
 
 end
